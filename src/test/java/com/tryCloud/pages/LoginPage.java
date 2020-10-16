e0f56464ce05c663b59081295879cf34be0a4224
sekercelik <sceliksidika@gmail.com>
2020-10-16 14:03:34 -0400

    LoginPage added

---
+++ src/test/java/com/tryCloud/pages/LoginPage.java
@@ -0,0 +1,25 @@
+package com.tryCloud.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends PageBase{
+    @FindBy(id = "user")
+    private WebElement username;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "submit")
+    private WebElement login_btn;
+
+    @FindBy(id = "lost-password")
+    private WebElement forgot_password;
+
+    @FindBy(className= "avatardiv avatardiv-shown")
+    private WebElement user_icon;
+
+    @FindBy(xpath = "//li[@data-id='logout']//a")
+    private WebElement log_out;
+
+}
