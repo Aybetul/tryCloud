94ff2ba49f303c0d1cc4468356cced84d2e8ba28
Sumeyra1! <smyr.math@gmail.com>
2020-10-21 12:19:15 -0400

    calendar page

---
+++ src/test/java/com/tryCloud/step_definitions/Calendar_StepDefs.java
@@ -0,0 +1,128 @@
+package com.tryCloud.step_definitions;
+
+import com.tryCloud.pages.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Calendar_StepDefs {
+    LoginPage login=new LoginPage();
+
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+    }
+
+    @When("user enters username and password")
+    public void user_enters_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks on {string} tab")
+    public void user_clicks_on_tab(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user clicks on {string} button")
+    public void user_clicks_on_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user clicks on New Calendar button")
+    public void user_clicks_on_New_Calendar_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user enters calender name")
+    public void user_enters_calender_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user clicks on arrow to select a month")
+    public void user_clicks_on_arrow_to_select_a_month() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user chooses a day form calender")
+    public void user_chooses_a_day_form_calender() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user types meeting name")
+    public void user_types_meeting_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user chooses calendar name from dropdown")
+    public void user_chooses_calendar_name_from_dropdown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user chooses starts day and time from popup calendar")
+    public void user_chooses_starts_day_and_time_from_popup_calendar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user chooses ends day and time from popup calendar")
+    public void user_chooses_ends_day_and_time_from_popup_calendar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user clicks on Create button")
+    public void user_clicks_on_Create_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify that meeting is created")
+    public void verify_that_meeting_is_created() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks New Calendar")
+    public void user_clicks_New_Calendar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user types a calendar name")
+    public void user_types_a_calendar_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks check sign")
+    public void user_clicks_check_sign() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user verifies calendar is created")
+    public void user_verifies_calendar_is_created() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks {string}")
+    public void user_clicks(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user verifies {string}")
+    public void user_verifies(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
